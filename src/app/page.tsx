33b0710f1505c92c8cea91365f7a6d5480798b51
Rohit <rohits8731@gmail.com>
2026-09-18 20:54:23 +0530

    03: Syncing User To Database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { LandingPage } from "@/features/home";
+import { syncUser } from "@/lib/actions/users";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
 
 const HomePage = async () => {
     const user = await currentUser();
+
+    await syncUser();
 
     if (user) redirect("/dashboard");
 
