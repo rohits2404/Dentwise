c41dfb0432c7e9abb4265a933247e04e60ebf799
Rohit <rohits8731@gmail.com>
2026-09-18 19:48:40 +0530

    01: Project Setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+    title: "DentWise - AI Powered Dental Assistant",
+    description:
+        "Get Instant Dental Advice Through Voice Calls With Our AI Assistant. Avaiable 24/7.",
+};
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+    return (
+        <html lang="en">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
+            >
+                {children}
+            </body>
+        </html>
+    );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <Button>Click Me</Button>
+        </div>
+    );
+};
+
+export default Home;
